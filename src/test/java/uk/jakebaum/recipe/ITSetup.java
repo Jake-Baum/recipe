59fc2d1858774c074d4874e498823b5841ff3cb4
Jake Baum <jake.jakebaum.uk>
2023-05-04 16:50:31 +0100

    Added version to test DB container.  Created Spring Boot profiles for testing and development

--- src/test/java/uk/jakebaum/recipe/ITSetup.java
+++ src/test/java/uk/jakebaum/recipe/ITSetup.java
@@ -3,8 +3,10 @@ package uk.jakebaum.recipe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("local-dev")
 public class ITSetup {
 
   @Autowired
